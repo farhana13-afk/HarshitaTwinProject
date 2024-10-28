fcf3c823c9d8fc2072f7b25c552c58b28ae7d9f2
harshita-sudhakar <harshita.sudhakar@gmail.com>
2024-10-28 13:37:17 -0400

    Coded CreateArray
    Coded the createArray method to randomize the numbers

--- HarshitaCodes.java
+++ HarshitaCodes.java
@@ -7,7 +7,13 @@ public class HarshitaCodes
 		int num = reader.nextInt();
 	}
 
-	public void createMethod () {
-
+	public void createArray (int num) {
+		int [][] numbers = new int [num][num];
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers[0].length; j++) {
+				int rand = (int) (Math.random()*10) + 1;
+				numbers[i][j] = rand;
+			}
+		}
 	}
 }
