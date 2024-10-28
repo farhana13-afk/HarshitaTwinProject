df3f73d51a685f9671dff5e0534236205b482ce4
harshita-sudhakar <harshita.sudhakar@gmail.com>
2024-10-28 13:41:24 -0400

    Called methods
    Called the print and create method

--- HarshitaCodes.java
+++ HarshitaCodes.java
@@ -4,10 +4,13 @@ public class HarshitaCodes
 	public static void main(String [] args)
 	{
 		Scanner reader = new Scanner (System.in);
+		System.out.print("give in a dimension: ");
 		int num = reader.nextInt();
+		int [][] array = createArray(num);
+		printArray(array);
 	}
 
-	public int[][] createArray (int num) {
+	public static int[][] createArray (int num) {
 		int [][] numbers = new int [num][num];
 		for (int i = 0; i < numbers.length; i++) {
 			for (int j = 0; j < numbers[0].length; j++) {
@@ -18,10 +21,10 @@ public class HarshitaCodes
 		return numbers;
 	}
 
-	public void printArray (int [][] numbers) {
+	public static void printArray (int [][] numbers) {
 		for (int i = 0; i < numbers.length; i++) {
 			for (int j = 0; j < numbers[0].length; j++) {
-				System.out.println(numbers[i][j] + " ");
+				System.out.print(" " + numbers[i][j] + " ");
 			}
 			System.out.println();
 		}
