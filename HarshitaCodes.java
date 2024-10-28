02e81d58e3b8da451dcf2fbbc45a927467900e19
harshita-sudhakar <harshita.sudhakar@gmail.com>
2024-10-28 13:38:57 -0400

    Created printArray
    Created and coded printArray method

--- HarshitaCodes.java
+++ HarshitaCodes.java
@@ -7,13 +7,23 @@ public class HarshitaCodes
 		int num = reader.nextInt();
 	}
 
-	public void createArray (int num) {
+	public int[][] createArray (int num) {
 		int [][] numbers = new int [num][num];
 		for (int i = 0; i < numbers.length; i++) {
 			for (int j = 0; j < numbers[0].length; j++) {
 				int rand = (int) (Math.random()*10) + 1;
 				numbers[i][j] = rand;
 			}
+		}
+		return numbers;
+	}
+
+	public void printArray (int [][] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers[0].length; j++) {
+				System.out.println(numbers[i][j] + " ");
+			}
+			System.out.println();
 		}
 	}
 }
