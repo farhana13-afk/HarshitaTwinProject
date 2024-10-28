b9e737c964f643fff55f08cd4eea6d1a3ba29eb8
harshita-sudhakar <harshita.sudhakar@gmail.com>
2024-10-28 13:34:20 -0400

    Deleted and Created Method
    Deleted askForNum method and created the create array methd

--- HarshitaCodes.java
+++ HarshitaCodes.java
@@ -3,11 +3,11 @@ public class HarshitaCodes
 {
 	public static void main(String [] args)
 	{
-
-	}
-	public int askForNum () {
 		Scanner reader = new Scanner (System.in);
 		int num = reader.nextInt();
-		return num;
+	}
+
+	public void createMethod () {
+
 	}
 }
